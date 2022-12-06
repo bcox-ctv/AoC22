c3751e50476612c1b7decf47a85c3d59b5cb5267
Brian Cox <bcox@captechconsulting.com>
2022-12-06 12:37:29 -0500

    12_06

--- 12_06/12_06.py
+++ 12_06/12_06.py
@@ -3,7 +3,6 @@ file1 = open('12_06/advent_6_input.txt')
 
 markerSize = 14
 for line in file1:
-    line = line.strip()
     for i, _ in enumerate(line):
         if len(set(line[i:i+markerSize:])) == markerSize:
             print(f'found {i+markerSize}')

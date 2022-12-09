f5e02937470769fffeff8c3b344e2b2050e3e7ae
Brian Cox <bcox@captechconsulting.com>
2022-12-09 14:33:02 -0500

    part 2

--- 12_08/12_08.py
+++ 12_08/12_08.py
@@ -17,8 +17,40 @@ def test_tree(i, j, forest):
 
     return visible_w or visible_e or visible_n or visible_s
 
+def visibility(i, j, forest):
+    forest_col = [row[j] for row in forest]
+    view_up = 0
+    for tree in forest_col[:i][::-1]:
+        view_up += 1
+        if tree >= forest[i][j]:
+            break
+
+    view_left = 0
+    for tree in forest[i][:j][::-1]:
+        view_left += 1
+        if tree >= forest[i][j]:
+            break
+
+    view_down = 0
+    for tree in forest_col[i+1:]:
+        view_down += 1
+        if tree >= forest[i][j]:
+            break
+
+    view_right  = 0
+    for tree in forest[i][j+1:]:
+        view_right += 1
+        if tree >= forest[i][j]:
+            break
+    return view_down * view_right * view_up * view_left
+
 counter = 0
+max_score = 0
 for i in range(len(forest)):
     for j in range(len(forest[i])):
         if test_tree(i, j, forest): counter += 1
+        vis_score = visibility(i,j,forest)
+        if vis_score > max_score: max_score = vis_score
+
 print(f"{counter} visible trees")
+print(f"{max_score} max scenic score")
